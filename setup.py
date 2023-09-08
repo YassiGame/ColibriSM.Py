857b4ebb6fa136eec88b706eae99280678d79217
YassiPC <yassertaabouz@gamil.com>
2023-09-08 21:57:32 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ColibriSM.Py',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/YassiGame/ColibriSM.Py',
+    license='MIT license',
+    author='YassiGame',
+    author_email='yassigame23@gmail.com',
+    description='ColibriSM.Py is a modern, easy to use and feature-rich API wrapper for ColibriSM (https://www.colibrism.ru/) '
+)
