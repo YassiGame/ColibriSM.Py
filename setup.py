d6973c9195e017a538627192312e2967ac37e52e
YassiPC <yassertaabouz@gamil.com>
2023-09-08 22:02:43 +0100

    + Update License

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=[''],
     url='https://github.com/YassiGame/ColibriSM.Py',
-    license='MIT license',
+    license='Apache License 2.0',
     author='YassiGame',
     author_email='yassigame23@gmail.com',
     description='ColibriSM.Py is a modern, easy to use and feature-rich API wrapper for ColibriSM (https://www.colibrism.ru/) '
